3aacfdf3daad531337a041adf9a0d7b81a3e84bd
Giovany Santos <giovany.m.s@hotmail.com>
2023-08-01 12:21:23 -0300

    feat #2.3: meta tags test

--- client/pages/layout.tsx
+++ client/pages/layout.tsx
@@ -30,7 +30,7 @@ const Meta: React.FC = () => {
   const { language } = useContext(LanguageContext);
   return (
     <Head>
-      <title>Giovany</title>
+      <title>Giovany 1</title>
       <meta name="language" content={language} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="author" content="Giovany Santos" />
