7d807452278264d2fd8f9a61efe82813a3ed2561
Giovany Santos <giovany.m.s@hotmail.com>
2023-08-03 17:50:52 -0300

    feat #2.15: meta tags

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from 'next';
 import './globals.css';
 
-export const metadata: Metadata = {
+const commons = {
   title: 'Giovany Santos',
-  description: 'Bem vindo ao portfolio de Giovany Santos',
+  description: 'Encontre aqui meu portifólio, carreira e meus contatos',
+  img: '/img/profile.jpg',
+};
+
+export const metadata: Metadata = {
+  title: commons.title,
+  description: commons.description,
   metadataBase: new URL('https://giovany.com.br'),
+  viewport: {
+    width: 'device-width',
+    initialScale: 1.0,
+  },
   keywords: [
     'Desenvolvimento de software',
     'Agência digital',
@@ -28,13 +38,13 @@ export const metadata: Metadata = {
   creator: 'Giovany Santos',
   publisher: 'Giovany Santos',
   openGraph: {
-    title: 'Giovany Santos',
-    description: 'Bem vindo ao portfolio de Giovany Santos',
+    title: commons.title,
+    description: commons.description,
     url: 'https://giovany.com.br',
     siteName: 'Giovany',
     images: [
       {
-        url: '/img/profile.jpg',
+        url: commons.img,
         width: 667,
         height: 1003,
       },
@@ -53,12 +63,18 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
+    title: commons.title,
+    description: commons.description,
     card: 'summary',
-    title: 'Giovany Santos',
-    description: 'Bem vindo ao portfolio de Giovany Santos',
     site: '@torradinha0',
     creator: '@torradinha0',
-    images: ['https://giovany.com.br/img/profile.png'],
+    images: [
+      {
+        url: commons.img,
+        width: 667,
+        height: 1003,
+      },
+    ],
   },
 };
 
@@ -73,50 +89,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// const Meta: React.FC = () => (
-//   <Head>
-//     <title>Giovany 1</title>
-//     {/* <meta name="language" content={language} /> */}
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <meta name="author" content="Giovany Santos" />
-//     <meta name="robots" content="max-image-preview:large" />
-//     <meta name="robots" content="index, follow" />
-//     {/* <meta name="title" content="Giovany Santos" /> */}
-//     {/* <meta
-//         name="description"
-//         content="Bem vindo ao portfolio de Giovany Santos"
-//       /> */}
-//     {/* <meta
-//         name="keywords"
-//         content="frontend, react, node, javascript, giovany, santos, graphql, programação, programador, desenvolvedor"
-//       /> */}
-
-//     <meta property="twitter:title" content="Giovany Santos" />
-//     <meta
-//       property="twitter:description"
-//       content="Desenvolvedor fullstack Giovany Santos"
-//     />
-//     <meta property="twitter:card" content="summary_large_image" />
-//     <meta
-//       property="twitter:image"
-//       content="https://unsplash.com/photos/UYsBCu9RP3Y"
-//     />
-
-//     {/* <meta property="og:title" content="Giovany Santos" />
-//       <meta
-//         property="og:description"
-//         content="Desenvolvedor fullstack Giovany Santos"
-//       /> */}
-//     {/* <meta property="og:url" content="https://www.giovany.com.br" />
-//       <meta
-//         property="og:image"
-//         content="https://unsplash.com/photos/UYsBCu9RP3Y"
-//       /> */}
-//     <meta property="og:type" content="article" />
-//     <meta property="og:site_name" content="Giovany Santos" />
-//     <link rel="icon" href="/favicon.ico" />
-//   </Head>
-// );
-
-// export default Meta;
